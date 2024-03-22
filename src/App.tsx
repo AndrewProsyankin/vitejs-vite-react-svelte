1cf3447f095610ec3b60fb4bdbe872dc53e54032
Andrew Prosyankin <prosyankin@gmail.com>
2024-03-22 14:55:45 +0200

    svelte and storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import AppSvelte from './App.svelte';
+import type { AriaTextFieldProps, TextFieldProps } from '@react-types/textfield';
+
+export interface PasswordFieldProps extends TextFieldProps {
+  onSubmit?: (value: string) => void;
+
+  onClear?: () => void;
+}
+
+export interface AriaPasswordFieldProps
+  extends PasswordFieldProps,
+    AriaTextFieldProps {}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!ref.current) return;
+    const app = new AppSvelte({
+      target: ref.current,
+    });
+    return () => app.$destroy();
+  }, []);
 
   return (
     <>
+      <div ref={ref}></div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href='https://vitejs.dev' target='_blank'>
+          <img src={viteLogo} className='logo' alt='Vite logo' />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href='https://react.dev' target='_blank'>
+          <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className='card'>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -25,11 +47,11 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className='read-the-docs'>
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svelte()],
+  resolve: {
+    alias: {
+      '@': path.resolve('./src/'),
+      $storybook: path.resolve('./.storybook/'),
+      shared: path.resolve('./src/shared/'),
+    },
+  },
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: {
+        index: 'src/main.tsx',
+      },
+      output: {
+        entryFileNames: '[name].js',
+      },
+    },
+  },
 })
